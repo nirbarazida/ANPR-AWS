ee67b46eb1922d253feb1582d37783816139371d
Nir Barazida <nir@dagshub.com>
2022-04-24 22:15:25 +0300

    update setup_app

--- fire-app/setup_app.sh
+++ fire-app/setup_app.sh
@@ -1,7 +1,7 @@
 # debug
-# set -o xtrace
+#set -o xtrace
 
-KEY_NAME="NAPR-AWS"
+KEY_NAME="NAPR-AWS`date "+%m%d%H%M%Y.%S"`"
 KEY_PEM="$KEY_NAME.pem"
 
 echo "create key pair $KEY_PEM to connect to instances and save locally"
@@ -10,7 +10,7 @@ aws ec2 create-key-pair --key-name $KEY_NAME --query "KeyMaterial" --output text
 # secure the key pair
 chmod 400 $KEY_PEM
 
-SEC_GRP="NAPR-AWS-sg"
+SEC_GRP="NAPR-AWS-sg-`$KEY_NAME`"
 
 echo "setup firewall $SEC_GRP"
 aws ec2 create-security-group   \
@@ -25,7 +25,7 @@ echo "My IP: $MY_IP"
 echo "setup rule allowing SSH access to $MY_IP only"
 aws ec2 authorize-security-group-ingress        \
     --group-name $SEC_GRP --port 22 --protocol tcp \
-    --cidr $MY_IP/32
+    --cidr '0.0.0.0/0'
 
 echo "setup rule allowing HTTP (port 5000) access to $MY_IP only"
 aws ec2 authorize-security-group-ingress        \
@@ -39,6 +39,7 @@ RUN_INSTANCES=$(aws ec2 run-instances   \
     --image-id $UBUNTU_20_04_AMI        \
     --instance-type t2.micro            \
     --key-name $KEY_NAME                \
+    --credit-specification CpuCredits=unlimited \
     --security-groups $SEC_GRP)
 
 INSTANCE_ID=$(echo $RUN_INSTANCES | jq -r '.Instances[0].InstanceId')
@@ -54,36 +55,29 @@ echo "New instance $INSTANCE_ID @ $PUBLIC_IP"
 
 echo "Create table"
 aws dynamodb create-table \
-    --table-name ParkingLot \
+    --table-name "ParkingLog-`$KEY_NAME`" \
     --attribute-definitions AttributeName=ticketId,AttributeType=S \
     --key-schema AttributeName=ticketId,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
 
-echo "Update apt"
+
+echo "Deploy app"
 ssh -tt -i $KEY_PEM -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
+    set -o xtrace
     sudo apt-get update -y
-EOF
-
-echo "upgrade apt"
-ssh -tt -i $KEY_PEM -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
     sudo apt-get upgrade -y
-EOF
-
-echo "Deploy code to machine"
-ssh -tt -i $KEY_PEM -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
     echo "install pip"
     sudo apt install python3-pip -y
     sudo pip3 install --upgrade pip
     echo "Clone repo"
-    sudo git clone https://github.com/nirbarazida/ANPR-AWS.git
+    git clone https://github.com/nirbarazida/ANPR-AWS.git
     cd ANPR-AWS
     echo "Install requirements"
-    sudo pip install -r requirements.txt
-EOF
-
-echo "Rub app"
-ssh -tt -i $KEY_PEM -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
+    pip3 install -r requirements.txt
     export FLASK_APP=app/app.py
     echo "Run app"
     python3 -m flask run --host=0.0.0.0
 EOF
+
+echo "test that it all worked"
+curl  --retry-connrefused --retry 10 --retry-delay 1  http://$PUBLIC_IP:5000
