9b670cf4272aa96f453bc915d7fddd16dbdbac72
Nir Barazida <nir@dagshub.com>
2022-04-28 16:40:09 +0300

    working bash script

--- fire-app/setup_app.sh
+++ fire-app/setup_app.sh
@@ -1,16 +1,17 @@
 # debug
 #set -o xtrace
 
-KEY_NAME="NAPR-AWS`date "+%m%d%H%M%Y.%S"`"
+KEY_NAME="NAPR-AWS-`date +'%N'`"
 KEY_PEM="$KEY_NAME.pem"
 
 echo "create key pair $KEY_PEM to connect to instances and save locally"
-aws ec2 create-key-pair --key-name $KEY_NAME --query "KeyMaterial" --output text > $KEY_PEM
+aws ec2 create-key-pair --key-name $KEY_NAME \
+    | jq -r ".KeyMaterial" > $KEY_PEM
 
 # secure the key pair
 chmod 400 $KEY_PEM
 
-SEC_GRP="NAPR-AWS-sg-`$KEY_NAME`"
+SEC_GRP="my-sg-`date +'%N'`"
 
 echo "setup firewall $SEC_GRP"
 aws ec2 create-security-group   \
@@ -21,18 +22,17 @@ aws ec2 create-security-group   \
 MY_IP=$(curl ipinfo.io/ip)
 echo "My IP: $MY_IP"
 
-
 echo "setup rule allowing SSH access to $MY_IP only"
 aws ec2 authorize-security-group-ingress        \
     --group-name $SEC_GRP --port 22 --protocol tcp \
-    --cidr '0.0.0.0/0'
+    --cidr $MY_IP/32
 
 echo "setup rule allowing HTTP (port 5000) access to $MY_IP only"
 aws ec2 authorize-security-group-ingress        \
     --group-name $SEC_GRP --port 5000 --protocol tcp \
     --cidr $MY_IP/32
 
-UBUNTU_20_04_AMI="ami-0d527b8c289b4af7f"
+UBUNTU_20_04_AMI="ami-042e8287309f5df03"
 
 echo "Creating Ubuntu 20.04 instance..."
 RUN_INSTANCES=$(aws ec2 run-instances   \
@@ -55,29 +55,76 @@ echo "New instance $INSTANCE_ID @ $PUBLIC_IP"
 
 echo "Create table"
 aws dynamodb create-table \
-    --table-name "ParkingLog-`$KEY_NAME`" \
+    --table-name "ParkingLog" \
     --attribute-definitions AttributeName=ticketId,AttributeType=S \
     --key-schema AttributeName=ticketId,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
 
 
-echo "Deploy app"
-ssh -tt -i $KEY_PEM -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
-    set -o xtrace
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
-    echo "install pip"
-    sudo apt install python3-pip -y
-    sudo pip3 install --upgrade pip
-    echo "Clone repo"
-    git clone https://github.com/nirbarazida/ANPR-AWS.git
-    cd ANPR-AWS
-    echo "Install requirements"
-    pip3 install -r requirements.txt
-    export FLASK_APP=app/app.py
-    echo "Run app"
-    python3 -m flask run --host=0.0.0.0
+echo "setup production environment"
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
+    sudo apt update
+    sudo apt install python3-flask -y
+    # run app
+    nohup flask run --host 0.0.0.0  &>/dev/null &
+    exit
 EOF
 
-echo "test that it all worked"
-curl  --retry-connrefused --retry 10 --retry-delay 1  http://$PUBLIC_IP:5000
+
+echo "Deploy app"
+ssh -tt -i $KEY_PEM -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP '\
+    sudo apt-get update -y ;\
+    echo "install pip" ;\
+    sudo apt install python3-pip -y ;\
+    sudo pip3 install --upgrade pip ;\
+    echo "Clone repo" ;\
+    git clone https://github.com/nirbarazida/ANPR-AWS.git ;\
+    cd ANPR-AWS ;\
+    echo "Install requirements" ;\
+    pip3 install -r requirements.txt ;\
+    export FLASK_APP=app/app.py ;\
+    echo "Run app" ;\
+    python3 -m flask run --host=0.0.0.0'
+
+
+
+
+
+
+
+#ssh -tt -i $KEY_PEM -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP \
+#    set -o xtrace
+#    sudo apt-get update -y
+#    sudo apt-get upgrade -y
+#    echo "install pip"
+#    sudo apt install python3-pip -y
+#    sudo pip3 install --upgrade pip
+#    echo "Clone repo"
+#    git clone https://github.com/nirbarazida/ANPR-AWS.git
+#    cd ANPR-AWS
+#    echo "Install requirements"
+#    pip3 install -r requirements.txt
+#    export FLASK_APP=app/app.py
+#    echo "Run app"
+#    python3 -m flask run --host=0.0.0.0
+
+
+
+
+#echo "Deploy app"
+#ssh -tt -i $KEY_PEM -o "IdentitiesOnly=yes" -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP '\
+#    sudo apt-get update -y ;\
+#    sudo apt-get upgrade -y ;\
+#    echo "install pip" ;\
+#    sudo apt-get install python3.8 -y ;\
+#    sudo apt-get install python3-setuptools -y ;\
+#    sudo apt-get install python3-pip -y ;\
+#    sudo pip3 install --upgrade pip -y ;\
+#    echo "Clone repo" ;\
+#    git clone https://github.com/nirbarazida/ANPR-AWS.git ;\
+#    cd ANPR-AWS ;\
+#    echo "Install requirements" ;\
+#    pip3 install -r requirements.txt ;\
+#    export FLASK_APP=app/app.py ;\
+#    echo "Run app" ;\
+#    python3 -m flask run --host=0.0.0.0'
